434502cdf1b6b978ab3f0176b1f70a5661cb72f5
Wilson Vicente <wilsonvicentelucas@gmail.com>
2024-01-27 00:01:41 +0100

    Recursos

---
+++ codeigniter4/app/Controllers/News.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controllers;
+use CodeIgniter\Controller;
+use App\Models\NewsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
+
+class News extends BaseController{
+
+    public function index(){
+
+        $model = model(NewsModel::class);
+
+        $data = [
+            'news' => $model->getNews(),
+            'title' => "Notícias do Banco de Dados",
+        ];
+
+        return view('templates/header', $data)
+                .view('news/index')
+                .view('templates/footer');
+    }
+
+    public function show($slug = null){
+
+        $model = new NewsModel();
+        $data['news'] = $model->getNews($slug);
+
+        if(empty($data['news'])){
+            throw new PageNotFoundException("Não encontramos essa notícia: ". $slug);
+        }
+
+        return view('templates/header', $data)
+                .view('news/view')
+                .view('templates/footer');
+    }
+}
